7d25a5465267aa1442b5da61a4c8e47885527a80
9erch <34993177+9erch@users.noreply.github.com>
2022-05-11 17:28:02 +0100

    Create About-us-page.js

---
+++ src/Model/About-us-page.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+function About_us_page() {
+  return (
+    
+    <Container>
+        <div className="blues title" >
+        <h3>About us</h3>
+    </div>
+    <Row >
+   <Col xs={6}>
+   <img src="about-us.png" width={600}/>
+  
+   </Col>
+  
+   <Col xs={6} className='class-about'>
+    <h3>Qui sommes-nous</h3>
+    <p>
+    La Fondation Khalil Al-Ghanimi a vu le jour à Riyad, où elle s'efforce de trouver des solutions techniques et des cadres humains spécialisés dotés d'une grande 
+    expertise pour contribuer à la réalisation de la transformation numérique et fournir des consultations par le biais d'une équipe de travail et de consultants distingués.
+    </p>
+  
+   </Col>
+  
+   </Row>
+   
+   </Container>  
+
+
+  )
+}
+
+export default About_us_page ;
